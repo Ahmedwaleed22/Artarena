0180926fe35d78dbb6c6bfbb1adb1cc71d2bfd5c
Ahmed Waleed <ahmedwaleedaboelamayem68@gmail.com>
2020-11-19 11:49:23 +0200

    Edited Frontend And Form Send Data To Database

--- src/gallery/models.py
+++ src/gallery/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
+import os
 
 # Create your models here.
+
+def get_file_path(instance, filename):
+    ext = filename.split('.')[-1]
+    random_number = User.objects.make_random_password(length=10, allowed_chars='1234567890')
+    EditedFileName = "%s.%s" % (random_number + '_' + filename, '')
+    return os.path.join('paintings', EditedFileName)
+
+
+class Painting(models.Model):
+    Painting = models.ImageField(upload_to=get_file_path)
+    PaintingName = models.CharField(max_length=255)
+    PaintingDesc = models.TextField()
+    PaintingCat = models.CharField(max_length=255)
+    Artist = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.PaintingName
--- src/gallery/views.py
+++ src/gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import Http404
+from .models import Painting
 
 # Create your views here.
 
@@ -10,7 +11,25 @@ class gallery(View):
 
 
 class add(View):
+    template = 'addphoto.html'
+    def post(self, request):
+        painting = request.FILES["file"]
+        paintingname = request.POST.get("name")
+        paintingdesc = request.POST.get("description")
+        paintingcategory = request.POST.get("category")
+
+        if painting != "" and paintingname != "" and paintingdesc != "" and paintingcategory != None:
+            addpainting = Painting(Painting=painting, PaintingName=paintingname,
+            PaintingDesc=paintingdesc, PaintingCat=paintingcategory, Artist=request.user)
+            addpainting.save()
+            
+            return render(request, self.template, {"MSG": "Your Brand New Painting Successfully Added"})
+
+        else:
+            return render(request, self.template, {"MSG": "Please Check That All Fields Are Not Empty"})
+
+
     def get(self, request):
         if not request.user.is_authenticated:
             raise Http404
-        return render(request, 'addphoto.html', {})
+        return render(request, self.template, {})
